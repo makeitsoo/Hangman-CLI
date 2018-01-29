5700384d351fcacd3b787f74f6b7e21f883ef350
Barry Williams <barrywilliams84@gmail.com>
2018-01-29 01:02:55 -0800

    first ccouple methods in Word constructor done

--- Word.js
+++ Word.js
@@ -12,7 +12,7 @@
 var Letter = require("./Letter");
 
 
-var Word = function() {
+var Word = function(werd) {
 	this.newLetter = new Letter();
 	this.letterArray = function() {
 		// take new letter and push into array representing underlying word
@@ -33,3 +33,35 @@ var Word = function() {
 }
 
 module.exports = Word;
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var Letter = require("./Letter");
+
+
+var Word = function(werd) {
+	// store word as string
+	this.word = werd;
+	this.letterArray = [];
+	this.wordGuessed = false;
+	// take new letter and push into array representing underlying word
+	this.pushToLetterArray = function() {
+		for (var i = 0; i < this.word.length; i++) {
+			var newLetter = new Letter(this.word[i]);
+			this.letterArray.push(newLetter);
+			console.log(letterArray);
+		}
+
+	}
+	// method that checks each letter in word to see if all letters have been guessed
+	this.hasBeenGuessed = function() {
+		if(this.letterArray.every(function(ltr) {
+			return ltr.guessed === true;
+		})) {
+			this.wordGuessed = true;
+			return true;
+		}
+	}
+
+}
