4c7686d45280cd39ddf53e4f6f9333c850947cf8
Barry Williams <barrywilliams84@gmail.com>
2018-01-28 23:56:02 -0800

    completed Letter constructor in Letter.js file

--- Letter.js
+++ Letter.js
@@ -1,47 +1,28 @@
-// * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+// * **Letter.js**: Contains a constructor, Letter. This constructor  either displays the underlying character or a blank placeholder (an underscore), depending on whether or not the user has guessed the letter. 
 
-//   * A string value to store the underlying character for the letter
-
-//   * A boolean value that stores whether that letter has been guessed yet
-
-//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-
-//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+/// Letter.js controls whether users guess is correct and if it is displays letter, else displays "_" 
 
 
-// `Letter.js` *should not* `require` any other files.
+// CONSTRUCTOR FUNCTION
+var Letter = function(lettr) {
+  // property to store the letter
+  this.letter = lettr;
+  // property/boolean if the letter can be shown
+  this.guessed = false;
+  // function (method)
+  this.checkLetter = function() {
+    if(this.letter == ' '){ /*renders a blank as it is*/
+      //makes sure that when the function checks if the word is found doesn't read the blank as false.
+      this.guessed = true;
+      return '  ';
+    }if(this.guessed === false){ /*if it doesn't guessed, it returns a ' _ '*/
+      return ' _ ';
+    } else{ /*otherwise it just guesseds as itself*/
+      return this.letter;
+    }
 
-// **HINT:** Write `Letter.js` first and test it on its own before moving on, then do the same thing with `Word.js`
-
-// **HINT:** If you name your letter's display function `toString`, JavaScript will call that function automatically whenever casting that object to a string (check out this example: https://jsbin.com/facawetume/edit?js,console) --> also see this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/toString
-
-
-/// All Letter function does is hold value of a letter, display either this letter or underscore when user guesses a letter
-
-
-
-// var Letter = function() {
-// 	this.letter = letter;
-// 	this.guessed = false;
-// 	this.printResult = function() {
-// 		// function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-// 	}
-// 	this.letterCheck = function(this.letter) {
-// 		// function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-// 	}
-// }
-
-// module.exports = Letter;
-
-
-
-var Letter = function(lett){
-	this.charac = lett;
-	this.appear = false;
-	this.letterRender = function(){
-		return !(this.appear) ? "_" : this.charac;
-	};
+  };
 };
 
-//export the constructor
+// export to use in word.js
 module.exports = Letter;
