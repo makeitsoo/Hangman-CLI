bdb19d3065ec07b2fd4736bcc289eb9c39118edd
Barry Williams <barrywilliams84@gmail.com>
2018-01-28 20:24:06 -0800

    js constructors coded out in both Letter.js and Word.js. Will need to continue to build out but basic properties and methods defined.

--- Letter.js
+++ Letter.js
@@ -10,3 +10,38 @@
 
 
 // `Letter.js` *should not* `require` any other files.
+
+// **HINT:** Write `Letter.js` first and test it on its own before moving on, then do the same thing with `Word.js`
+
+// **HINT:** If you name your letter's display function `toString`, JavaScript will call that function automatically whenever casting that object to a string (check out this example: https://jsbin.com/facawetume/edit?js,console) --> also see this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/toString
+
+
+/// All Letter function does is hold value of a letter, display either this letter or underscore when user guesses a letter
+
+
+
+// var Letter = function() {
+// 	this.letter = letter;
+// 	this.guessed = false;
+// 	this.printResult = function() {
+// 		// function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+// 	}
+// 	this.letterCheck = function(this.letter) {
+// 		// function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+// 	}
+// }
+
+// module.exports = Letter;
+
+
+
+var Letter = function(lett){
+	this.charac = lett;
+	this.appear = false;
+	this.letterRender = function(){
+		return !(this.appear) ? "_" : this.charac;
+	};
+};
+
+//export the constructor
+module.exports = Letter;
--- Word.js
+++ Word.js
@@ -8,3 +8,28 @@
 
 
 // `Word.js` *should only* require `Letter.js`
+
+var Letter = require("./Letter");
+
+
+var Word = function() {
+	this.newLetter = new Letter();
+	this.letterArray = function() {
+		// take new letter and push into array representing underlying word
+		var letterArr = [];
+		letterArr.push(this.newLetter);
+		console.log(letterArr);
+	}
+	this.printWordString = function() {
+		// function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+	}
+	this.newLetterCheck = function(letter) {
+		var newLetterCheck = new Letter(letter);
+		// function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+		newLetterCheck.LetterCheck();
+		console.log(newLetterCheck.LetterCheck());
+
+	}
+}
+
+module.exports = Word;
